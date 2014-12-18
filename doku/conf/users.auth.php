c4be781b0c6b2dda6afd1e2028af2fe42ec3089d
LaraMarinha <laramarinha@gmail.com>
2014-12-18 15:27:16 +0000

    Deloitte add, schedule page and speakers

---
+++ doku/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+aaguiar:$1$weYzCfPI$8M68Jf8F.DzN5Ppvi3uzh0:Ademar Manuel Teixeira de Aguiar:ademar.aguiar@gmail.com:admin,user
