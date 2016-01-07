2fb082f76f47f5fcedea186a8ab058fd0d83a63e
Leonel Araújo <leonel.araujo27@gmail.com>
2016-01-07 17:13:54 +0000

    responsive make a bit#1, added plus sign to indicate that a talk has a description available

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,16 @@ function handleTopNavAnimation() {
 }
 
 smoothScroll.init();
+
+// Make a Bit #1 div's height is equal to 4 of the left-side-morning events
+$(window).on('resize', function(){
+    var firstEntry = $('#entry3').height();
+	var secondEntry = $('#entry4').height();
+	var thirdEntry = $('#entry5').height();
+	var forthEntry = $('#entry6').height();
+	var margins = 4;
+
+	$('#entry101').height(firstEntry + secondEntry + thirdEntry + forthEntry);
+	$('#entry101 .schedule-entry-title').height(firstEntry + secondEntry + thirdEntry + forthEntry - 4 * margins)
+});
+$(window).trigger('resize');
