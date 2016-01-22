4c269595b014e94ab4f99d68e080b7bef06f6e06
Leonel Araújo <leonel.araujo27@gmail.com>
2016-01-22 14:58:16 +0000

    guilherme duarte schedule data added. new check in layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,24 +20,37 @@ function handleTopNavAnimation() {
 smoothScroll.init();
 
 // Make a Bit #1 div's height is equal to 4 of the left-side-morning events
+
 $(window).on('resize', function(){
-    var firstEntry = $('#entry2').height();
-	var secondEntry = $('#entry3').height();
-	var thirdEntry = $('#entry4').height();
-	var fourthEntry = $('#entry5').height()/2;
-	var margins = 4;
+	if($(window).width() > 768){
+		var margins = 4;
 
-	$('#entry101').height(firstEntry + secondEntry + thirdEntry + fourthEntry);
-	$('#entry101').css('margin-bottom',fourthEntry*1.5);
-	// Setting heigh of the description of the Make a Bit #1 entry so that the vertical blue line has the proper height
-	$('#entry101 .schedule-entry-title').height(firstEntry + secondEntry + thirdEntry + fourthEntry*2)
+		// Make a Bit (right side) check in defines the height of the General Check in (left side)
+		var mabCheckIn = $('#entry101').height()
+		// Setting height of left side Check in which is longer than the right side one
+		$('#entry1').height(mabCheckIn * 1.25);
+		// Setting heigh vertical blue line height
+		$('#entry1 .schedule-entry-title').height(mabCheckIn)
 
-	// in mobile we want Make a Bit #1 height to be equal do Make a Bit#2
-	if($(window).width() < 768){
-		$('#entry101').css('margin-bottom',0);
-		$('#entry101').height( $('#entry102').height() );
-		$('#entry101 .schedule-entry-title').height( $('#entry102 .schedule-entry-title').height());
-	}
+		var firstEntry = $('#entry1').height();
+		var secondEntry = $('#entry2').height();
+		var thirdEntry = $('#entry3').height();
+		var fourthEntry = $('#entry4').height();
+		var fifthEntry = $('#entry5').height();
+		var sixthEntry = $('#entry6').height();
+		var seventhEntry = $('#entry7').height();
+
+		// Make a Bit #1 div's height is equal to 4 of the left-side-morning events and 0.2 of the left side checkin
+		$('#entry102').height(mabCheckIn * 0.25 + secondEntry + thirdEntry + fourthEntry + fifthEntry);
+		// Setting heigh vertical blue line height
+		$('#entry102 .schedule-entry-title').height(firstEntry * 0.25 + secondEntry + thirdEntry + fourthEntry + fifthEntry - margins*6)
+
+		// Make a Bit #2 div's height is equal to 2 of the left-side-morning events
+		$('#entry103').height(sixthEntry + seventhEntry);
+		// Setting heigh vertical blue line height
+		$('#entry103 .schedule-entry-title').height(sixthEntry + seventhEntry  - margins*4)
+
+	}	
 });
 $(window).trigger('resize');
 
