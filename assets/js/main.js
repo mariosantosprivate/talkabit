198c4450282018e61252facc1eba9c5d123f201b
Maria Marques <mariajoao.rmarques@gmail.com>
2016-12-05 20:35:48 +0000

    missing files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,9 @@
-var twitterLogo = document.getElementById('twitter');
-twitter.addEventListener('click', function() {
-  console.log('clicked!');
-  window.open('https://twitter.com/talkabitconf');
-});
-
-var facebookLogo = document.getElementById('facebook');
-facebookLogo.addEventListener('click', function() {
-  window.open('https://www.facebook.com/talkabit/');
-});
 
 var callForSponsors = document.getElementById('call-sponsors');
 callForSponsors.addEventListener('click', function() {
   window.open('assets/sponsorship.pdf');
 });
-
-var ring = document.getElementById('ring');
+/*var ring = document.getElementById('ring');
 
 var toggle = false;
 function rotateRing() {
@@ -26,3 +15,4 @@ function rotateRing() {
 
 setTimeout(rotateRing, 0);
 setInterval(rotateRing, 5000);
+*/
