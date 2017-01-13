c158a901d14bbc34977eb9a6dd015f2b0ca7cc55
Maria Marques <mariajoao.rmarques@gmail.com>
2017-01-13 17:35:38 +0000

    added speakers and schedule sections

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -8,16 +8,7 @@
     t("a.page-scroll").bind("click", function (a) {
         var o = t(this);
         t("html, body").stop().animate({
-            scrollTop: t(o.attr("href")).offset().top - 50
+            scrollTop: t(o.attr("href")).offset().top - 0
         }, 1250, "easeInOutExpo"), a.preventDefault()
-    }), t("body").scrollspy({
-        target: ".navbar-fixed-top",
-        offset: 51
-    }), t(".navbar-collapse ul li a").click(function () {
-        t(".navbar-toggle:visible").click()
-    }), t("#mainNav").affix({
-        offset: {
-            top: 100
-        }
     })
 }(jQuery);
