472968a2f8e9d923ce6b75780d6fa919569a0383
João Bernardino <joao.mnb@gmail.com>
2017-10-20 10:20:25 +0100

    Landing Page

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,14 +1,8 @@
-$('#countdown').countdown('2018/01/20 10:00:00')
+$('.countdown').countdown('2018/01/20 10:00:00')
 .on('update.countdown', function(event) {
-	$(this).find('.counter.weeks .value').text(event.offset.weeks);
-	$(this).find('.counter.days .value').text(event.offset.days);
-	$(this).find('.counter.hours .value').text(event.offset.hours);
-	$(this).find('.counter.minutes .value').text(event.offset.minutes);
-	$(this).find('.counter.seconds .value').text(event.offset.seconds);
-  // var $this = $(this).html(event.strftime(''
-  //   + '<span>%w</span> weeks '
-  //   + '<span>%d</span> days '
-  //   + '<span>%H</span> hr '
-  //   + '<span>%M</span> min '
-  //   + '<span>%S</span> sec'));
+	$(this).find('.counter.weeks .value').text(('0' + event.offset.weeks).slice(-2));
+	$(this).find('.counter.days .value').text(('0' + event.offset.days).slice(-2));
+	$(this).find('.counter.hours .value').text(('0' + event.offset.hours).slice(-2));
+	$(this).find('.counter.minutes .value').text(('0' + event.offset.minutes).slice(-2));
+	$(this).find('.counter.seconds .value').text(('0' + event.offset.seconds).slice(-2));
 });
