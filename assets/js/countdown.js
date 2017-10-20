28a30387de0f2214b45adca4162e482aef8468a1
João Bernardino <joao.mnb@gmail.com>
2017-10-20 01:57:52 +0100

    Starting cleanup

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,9 +1,14 @@
-$('#clock').countdown('2018/01/20 10:00:00')
+$('#countdown').countdown('2018/01/20 10:00:00')
 .on('update.countdown', function(event) {
-  var $this = $(this).html(event.strftime(''
-    + '<span>%w</span> weeks '
-    + '<span>%d</span> days '
-    + '<span>%H</span> hr '
-    + '<span>%M</span> min '
-    + '<span>%S</span> sec'));
+	$(this).find('.counter.weeks .value').text(event.offset.weeks);
+	$(this).find('.counter.days .value').text(event.offset.days);
+	$(this).find('.counter.hours .value').text(event.offset.hours);
+	$(this).find('.counter.minutes .value').text(event.offset.minutes);
+	$(this).find('.counter.seconds .value').text(event.offset.seconds);
+  // var $this = $(this).html(event.strftime(''
+  //   + '<span>%w</span> weeks '
+  //   + '<span>%d</span> days '
+  //   + '<span>%H</span> hr '
+  //   + '<span>%M</span> min '
+  //   + '<span>%S</span> sec'));
 });
