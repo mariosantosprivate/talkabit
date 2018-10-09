0b3c3ee318f9f60ba6238df72ce8655509265e5a
Pedro Lima <pdrlima96@hotmail.com>
2018-10-09 15:50:24 +0100

    Navbar improved.

--- js/hide-navbar.js
+++ js/hide-navbar.js
@@ -3,12 +3,14 @@ window.addEventListener('scroll', function (e) {
 
     if (window.pageYOffset === 0)
     {
+        document.getElementById("navbar").classList.remove("fadeIn");
         document.getElementById("navbar").classList.add("hide");
-        setTimeout(function(){ document.getElementById("navbar").classList.add("hide-first"); }, 700 - 700*0.1);
+        document.getElementById("navbarBrandImg").classList.add("hide");
     }
     else if (window.pageYOffset > 10)
     {
-        document.getElementById("navbar").classList.remove("hide-first");
+        document.getElementById("navbar").classList.add("fadeIn");
         document.getElementById("navbar").classList.remove("hide");
+        document.getElementById("navbarBrandImg").classList.remove("hide");
     }
 });
