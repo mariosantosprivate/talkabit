64086737d66752cc0aa71945ef99e1c485e1fd32
Pedro Lima <pdrlima96@hotmail.com>
2018-10-09 17:23:14 +0100

    Active link is automatically updated. Other sections must be created so the missing links can be restored.

--- js/change-active-link.js
+++ js/change-active-link.js
@@ -1,4 +1,20 @@
-$( '#navbarNav .navbar-nav li a' ).on( 'click', function () {
-    $( '#navbarNav .navbar-nav' ).find( 'li a.active' ).removeClass( 'active' );
-    $( this ).addClass( 'active' );
+$( document ).ready(function() {
+    $(document).on("scroll", onScroll);
+
+    function onScroll(event){
+
+        let scrollPos = $(document).scrollTop() + 1; // 1 the magic number
+        $('#navbarNav .navbar-nav li a').each(function () {
+            let currLink = $(this);
+            let refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $( '#navbarNav .navbar-nav' ).find( 'li a.active' ).removeClass( 'active' );
+                currLink.addClass('active');
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
+    }
 });
+
