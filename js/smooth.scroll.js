9d9389fd29e7f8c45651c3adb1706c0b510e2577
Andreia <andreia.c.rodrigues@inesctec.pt>
2018-10-13 01:57:14 +0100

    fix de muitas coisas, bootstrap e fontawesome agora ja estao no projeto, algumas coisas estão comentadas por ainda nao estarem feitas

--- js/smooth.scroll.js
+++ js/smooth.scroll.js
@@ -2,6 +2,6 @@ $(document).on('click', 'a.nav-link[href^="#"]', function (event) {
     event.preventDefault();
 
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 800);
+        scrollTop: $($.attr(this, 'href')).offset().top - 55
+    }, 900);
 });
